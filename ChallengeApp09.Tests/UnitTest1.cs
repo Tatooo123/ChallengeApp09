37fc35c9b4264a88c2692de50eae870f1852bbbc
Tatooo123 <stankowski@wr.onet.pl>
2024-01-08 21:43:23 +0100

    Day 9 wrkAdd project files.

---
+++ ChallengeApp09.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ChallengeApp09.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
