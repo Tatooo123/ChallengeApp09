eaca68f45518dddce8738d26a5e83161d8c41cc5
Tatooo123 <stankowski@wr.onet.pl>
2024-01-16 00:50:57 +0100

    Day 10 Homework v2

--- ChallengeApp09/Program.cs
+++ ChallengeApp09/Program.cs
@@ -1,68 +1,38 @@
 ﻿using ChallengeApp09;
 
-Console.WriteLine("------------------");
-Console.WriteLine("Hello, I'am Tatooo");
-Console.WriteLine("Program started   ");
-Console.WriteLine("------------------");
+//Console.WriteLine("------------------");
+//Console.WriteLine("Hello, I'am Tatooo");
+//Console.WriteLine("Program started   ");
+//Console.WriteLine("------------------");
 
-Employee worker0 = new Employee("Dorothy", "Longbutton", 41);
-Employee worker1 = new Employee("Tom", "Jones", 24);
-Employee worker2 = new Employee("Susie", "Jones", 29);
-Employee worker3 = new Employee("Mark", "Kowalski", 39);
-float maxScore = -1;
-int maxScoreWorkerIndex = -1;
-// Employee maxScoreWorker = null;
-List<Employee> workers = new List<Employee>()
-{
-    worker0, worker1, worker2, worker3 
-};
-worker0.AddPoints(1);
-worker0.AddPoints(4);
+Employee worker0 = new Employee("Frank", "Longbutton", 41);
+Console.WriteLine("Worker:");
+Console.WriteLine(" " + worker0.FirstName.PadRight(8, ' ') + " " + worker0.LastName.PadRight(12, ' ') + " age " + worker0.Age.ToString().PadLeft(3, ' '));
+Console.WriteLine("  - AddPoints(10.5F) ");
 worker0.AddPoints(10.5F);
+Console.WriteLine("  - AddPoints(300) ");
+worker0.AddPoints(300);
+Console.WriteLine("  - AddPoints("+'"'+99+'"'+") ");
+worker0.AddPoints("99");
+Console.WriteLine("  - AddPoints(" + '"' + -101 + '"' + ") ");
+worker0.AddPoints("-101");
+Console.WriteLine("  - AddPoints(3) ");
 worker0.AddPoints(3);
-worker0.AddPoints(6);
+Console.WriteLine("  - AddPoints(" + '"' + "ola" + '"' + ") ");
+worker0.AddPoints("ola");
 
-worker1.AddPoints(10);
-worker1.AddPoints(7);
-worker1.AddPoints(2);
-worker1.AddPoints(1);
-worker1.AddPoints(5);
-
-worker2.AddPoints(1);
-worker2.AddPoints(8);
-worker2.AddPoints(5);
-worker2.AddPoints(3);
-worker2.AddPoints(-9.5F);
-
-worker3.AddPoints(3);
-worker3.AddPoints(6.5F);
-worker3.AddPoints(1.5F);
-worker3.AddPoints(9);
-worker3.AddPoints(7);
-
-int ii = 0;
-foreach (var worker in workers)
-{
-    if (worker.Result > maxScore)
-    { 
-        maxScore = worker.Result;
-        maxScoreWorkerIndex = ii;
-    }
-    Console.WriteLine($"{ii:N0}" + " - " + workers[ii].FirstName.PadRight(8, ' ') + " " + workers[ii].LastName.PadRight(12, ' ') + $" age {workers[ii].Age:N0}" + $" score: {workers[ii].Result:N1} ");
-    ii++;
-}
 Console.WriteLine("------------------");
-Console.WriteLine("Max score worker is:");
-Console.WriteLine(maxScoreWorkerIndex.ToString().PadLeft(3, ' ') + " - " + workers[maxScoreWorkerIndex].FirstName.PadRight(8, ' ') + " " + workers[maxScoreWorkerIndex].LastName.PadRight(12, ' ') + " age " + workers[maxScoreWorkerIndex].Age.ToString().PadLeft(3, ' ') + " score: " + maxScore.ToString().PadLeft(3, ' '));
+Console.WriteLine("Worker sumary:");
+Console.WriteLine(" - " + " score: " + worker0.Result.ToString().PadLeft(3, ' '));
 
-var statistics = workers[maxScoreWorkerIndex].GetStatistics();
-Console.WriteLine(" - statistics for him:");
+var statistics = worker0.GetStatistics();
+Console.WriteLine(" - and statistics:");
 Console.WriteLine($"    Min:     {statistics.Minimum:N2}");
 Console.WriteLine($"    Max:     {statistics.Maximum:N2}");
 Console.WriteLine($"    Average: {statistics.Average:N2}");
 
 Console.WriteLine(" - and his punctation:");
-List<float> points = workers[maxScoreWorkerIndex].GetPointList();
+List<float> points = worker0.GetPointList();
 
 foreach (var score in points)
 {
